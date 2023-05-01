fcf35844936880870d72fc90009b12923232c348
Shaju Mohammed <shaju.mohammed@keoic.com>
2023-05-01 09:06:47 +0000

    test commit

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -12,7 +12,7 @@ const EXAMPLES: ExampleModel[] = [
         text: "what are the capabilities in the organization?",
         value: "what are the capabilities in KEO International Consultants?"
     },
-    { text: "What is the scope of work in Riyadh - Sedra Phase 3, 4 and 5?", value: "What is the scope of work in Riyadh - Sedra Phase 3, 4 and 5?" },
+    { text: "What are the proposals that were submitted?", value: "What are the project proposals available in the sources?" },
     { text: "What does a Project Manager do?", value: "What does a Project Manager do?" }
 ];
 
